1b717ad2b4c17e54d1c6d01165d1b560c30c025f
karadikid <goukaradi@hotmail.com>
2020-12-07 10:00:58 -0500

    Successful objectDetector.detect after adding modelLoaded promise.

--- sketch.js
+++ sketch.js
@@ -1,5 +1,27 @@
+let img;
+let objectDetector;
+
+function preload() {
+	img = loadImage('./images/gty_cat_dog_portrait.jpg');
+	objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+
+function gotDetections(error, results) {
+	if (error) {
+		console.log(error);
+	}
+	console.log(results);
+}
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(640, 480);
+	image(img, 0, 0);
+	console.log(objectDetector)
+	objectDetector.detect(img, gotDetections);
 }
 
 function draw() {
