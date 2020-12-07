3bde7ea1df631b21d3634acadce7c40ffd17d6c1
karadikid <goukaradi@hotmail.com>
2020-12-07 11:51:45 -0500

    Finally got noFill to work. Wrong capitalization!

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 let img;
 let objectDetector;
+let object;
 
 function preload() {
 	img = loadImage('./images/gty_cat_dog_portrait.jpg');
@@ -15,12 +16,18 @@ function gotDetections(error, results) {
 		console.log(error);
 	}
 	console.log(results);
+	for (i = 0; i < results.length; i++) {
+		let object = results[i];
+		stroke(0, 255, 0);
+		strokeWeight(4);
+		noFill();
+		rect(object.x, object.y, object.width, object.height)
+	}
 }
 
 function setup() {
 	createCanvas(640, 480);
 	image(img, 0, 0);
-	console.log(objectDetector)
 	objectDetector.detect(img, gotDetections);
 }
 
