5e277766814e58cdae2f53b82bf005aa04ba1546
karadikid <goukaradi@hotmail.com>
2020-12-07 12:43:57 -0500

    Successfully fixed setTimeout, global variable for results in order to move rendering to draw function.

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 let video;
 let objectDetector;
 let object;
+let detections = [];
 
 function preload() {
 	img = loadImage('./images/gty_cat_dog_portrait.jpg');
@@ -15,9 +16,24 @@ function gotDetections(error, results) {
 	if (error) {
 		console.log(error);
 	}
-	console.log(results);
-	for (i = 0; i < results.length; i++) {
-		let object = results[i];
+	detections = results;
+		//setTimeout (() => { console.log(detections.label); }, 500);
+		//objectDetector.detect(video, gotDetections);
+		setTimeout(() => { objectDetector.detect(video, gotDetections); }, 500);
+	}
+
+function setup() {
+	createCanvas(640, 480);
+	video = createCapture(VIDEO);
+	video.size(640, 480);
+	objectDetector.detect(video, gotDetections);
+}
+
+function draw() {
+	image(video, 0, 0);
+
+	for (i = 0; i < detections.length; i++) {
+		let object = detections[i];
 		stroke(0, 255, 0);
 		strokeWeight(4);
 		noFill();
@@ -31,19 +47,5 @@ function gotDetections(error, results) {
 		textSize(20);
 		let confidence = nf(object.confidence, 0, 3);
 		text(confidence, object.x+60, object.y+150);
-		setTimeout (() => { console.log(object.label); }, 500);
-		objectDetector.detect(video, gotDetections);
-		//setTimeout(() => { objectDetector.detect(video, gotDetections); }, 200);
 	}
-}
-
-function setup() {
-	createCanvas(640, 480);
-	video = createCapture(VIDEO);
-	video.size(640, 480);
-	objectDetector.detect(video, gotDetections);
-}
-
-function draw() {
-	image(video, 0, 0);
 }
